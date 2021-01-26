ccea1c8673a57db805a71d0bab6b59c21b80b035
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-01-26 15:01:22 +0100

    adding     useNewUrlParser: true,

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 dotenv.config();
 
-
-
 //database
 
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
     console.log("Connected to database");
-});
+  });
 
 //routes//
-const postRoutes = require('./routes/post');
-const userRoutes = require('./routes/user');
+const postRoutes = require("./routes/post");
+const userRoutes = require("./routes/user");
 
 //middleware//
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-app.use('/api', postRoutes);
-app.use('/api', userRoutes);
-
+app.use("/api", postRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
-        console.log("Server listening on port 8000");
-    })
+  console.log("Server listening on port 8000");
+});
